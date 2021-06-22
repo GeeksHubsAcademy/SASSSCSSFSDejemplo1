75fea571372200f6a8576042cdcfa3a52a6938b1
Dave86dev <davidochando1986@gmail.com>
2021-06-22 13:36:05 +0200

    Ejemplo implementacion SASS SCSS

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="vistaHome">
+            <div className="tarjeta">
+                soy una tarjeta dentro de Home
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/containers/Login/Login.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            soy Login
+        </div>
+    )
+}
+
+export default Login;
